5ec65c917e47b2649e2f51f252ab62a153bc4792
hanyuwei70@ <hanyuwei70@qq.com>
2015-03-08 00:28:01 +0800

    日常编写css

--- index.php
+++ index.php
@@ -7,7 +7,31 @@
 <meta charset="UTF-8" />
 <title><?php echo $WEBSITE_TITLE?></title>
 <link rel="stylesheet" type="text/css" href="mainstyle.css" />
+<link rel="icon" type="image/vnd-microsoft.icon" href="favicon.ico" />
 </head>
+<div id="container">
+	<div id="header">
+	</div>
+	<div id="content">
+		<div id="logo" class="title">
+		<h1>北邮檐枫动漫社</h1><span class="sectitle">bupt.moe 北邮人的作死天堂</span>
+		</div>
+		<div id="post">
+			<div id="post-1" class="posts">
+				<div class="posttitle">
+					<h2>A Test Post</h2>
+				</div>
+				<div class="postcontent-pre">
+					标题说的还不够清楚么！
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+	<p>bupt.moe 由 hanyuwei70 设计</p>
+	<p>联系：hanyuwei70#qq.com</p>
+	</div>
+</div>
 <body>
 </body>
 </html>
